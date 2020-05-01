8588396dd94fe3c3dc3e791846cb1d9316ad5827
AJ Raymond <antraymo@iu.edu>
2020-04-30 20:12:32 -0400

    responsive styles

--- evanoneal/components/VidScroller.js
+++ evanoneal/components/VidScroller.js
@@ -17,24 +17,166 @@ export default class VidScroller extends React.Component {
                 ))}
 
                 <style jsx>{`
-                    .grid-container {
-                        display: grid;
-                        grid-gap: 10px;
-                        grid-template-columns: repeat(100, 40%) 5px; 
-                        grid-template-rows: minmax(150px, 1fr);
-                        overflow-x: scroll;
-                        padding-bottom: 10px;
-                        margin-bottom: 10px;
-                        align-content: start;
-                        overflow-y: hidden; 
-                        overflow-x: scroll;
+                    /* desktop */
+                    @media (min-device-width:1224px) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 45%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }    
                     }
 
-                    .grid-container > div {
-                        text-align: center;
-                        padding: 10px 0;
-                        font-size: 30px;
+                    /* large desktop screen */
+                    @media (min-device-width:1824px) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 45%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
                     }
+
+                    /* Small desktop screen */
+                    @media (max-width:1224px) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 45%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
+                    }
+
+                    /* Tablet Device */
+                    @media (max-device-width:1224px) {
+                    @media (orientation: portrait) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 70%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
+                    }
+                    @media (orientation: landscape) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 70%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
+                    }
+                    }
+
+                    /* Mobile */
+                    @media (max-device-width:850px) {
+                    @media (orientation: portrait) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 85%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
+                    }
+                    @media (orientation: landscape) {
+                        .grid-container {
+                            display: grid;
+                            grid-gap: 10px;
+                            grid-template-columns: repeat(100, 40%) 5px; 
+                            grid-template-rows: minmax(150px, 1fr);
+                            overflow-x: scroll;
+                            padding-bottom: 10px;
+                            margin-bottom: 25px;
+                            align-content: start;
+                            overflow-y: hidden; 
+                            overflow-x: scroll;
+                        }
+
+                        .grid-container > div {
+                            text-align: center;
+                            padding: 10px 0;
+                            font-size: 30px;
+                        }
+
+                    }
+                    }
+                  
                 `}</style>
             </div>
             </div>
