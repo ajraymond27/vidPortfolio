68b9db4eb4e8b9d1a2e76891347659ce82db1859
AJ Raymond <antraymo@iu.edu>
2020-05-04 16:26:54 -0400

    small update

--- evanoneal/pages/contact.js
+++ evanoneal/pages/contact.js
@@ -1,23 +1,49 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTv } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
 
+
+const icon={
+  margin: '50px',
+}
 
 const Contact = () => (
   <>
 
   <Header />
   <div className="page"> 
-    <h1>Contact</h1>
-    <FontAwesomeIcon icon={faTv}/>
-    <span>evanjoneal@gmail.com</span>
-    
-    <FontAwesomeIcon icon={faTv}/>
-    <span>555-555-5555</span>
- 
+  <h1>Contact</h1>
+    <Card >
+      <Card.Body>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
+        <span>         evanjoneal@gmail.com</span>
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
+        <span>      (317) 556-5848</span>
+      </Card.Subtitle>
+      </Card.Body>
+    </Card>
+
     <h1>Connect</h1>
+    <Card >
+      <Card.Body>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
+        <span>         evanjoneal@gmail.com</span>
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
+        <span>      (317) 556-5848</span>
+      </Card.Subtitle>
+      </Card.Body>
+    </Card>
+ 
+    {/* <h1>Connect</h1>
     <FontAwesomeIcon icon={faTv}/>
     <span>LinkedIn Link</span>
     
@@ -25,7 +51,7 @@ const Contact = () => (
     <span>Instagram Link</span>
 
     <FontAwesomeIcon icon={faTv}/>
-    <span>Facebook Link</span>
+    <span>Facebook Link</span> */}
   </div>
   <Footer />
 
