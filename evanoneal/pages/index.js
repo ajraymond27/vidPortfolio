812b0e107b8003fa1a6879136d69100126818d1c
AJ Raymond <antraymo@iu.edu>
2020-05-04 12:51:01 -0400

    dynamic reel vid

--- evanoneal/pages/index.js
+++ evanoneal/pages/index.js
@@ -12,12 +12,12 @@ import Header from '../components/Header';
 import VidScroller from '../components/VidScroller';
 import Footer from '../components/Footer';
 
-const reel ={
-  width: "100%",
-  height: 'auto',
-  margin: '0px 0px 20px 0px',
-  textAlign: 'center',
-}
+// const reel ={
+//   width: "100%",
+//   height: '50vh',
+//   margin: '0px 0px 20px 0px',
+//   textAlign: 'center',
+// }
 
 const vid ={
   margin: '0 auto',
@@ -35,18 +35,18 @@ const button ={
   width: '250px',
 }
 
-const evan ={
-  width: '100%',
-  // borderTop: 'solid 1px var(--shadow)',
-  // borderBottom: 'solid 1px var(--shadow)',
-  padding: '10px 0px 10px 0px',
-  marginTop: 10,
-}
+// const evan ={
+//   width: '100%',
+//   // borderTop: 'solid 1px var(--shadow)',
+//   // borderBottom: 'solid 1px var(--shadow)',
+//   padding: '10px 0px 10px 0px',
+//   marginTop: 10,
+// }
 
-const icon ={
-  fontSize: '100pt',
-  color: 'var(--base1)',
-}
+// const icon ={
+//   fontSize: '100pt',
+//   color: 'var(--base1)',
+// }
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -59,8 +59,8 @@ export default class Home extends React.Component {
     return (
       <>
         <Header />
-        <div style={reel}>
-          <ReactPlayer style={vid} url='https://www.youtube.com/watch?v=YtCy8ecMBeQ' loop controls width={'100%'} height={'90vh'}/>
+        <div className="reel">
+          <ReactPlayer style={vid} url='https://www.youtube.com/watch?v=YtCy8ecMBeQ' loop controls width={'100%'} height={'100%'}/>
         </div>
 
         <div className="page"> 
@@ -69,7 +69,7 @@ export default class Home extends React.Component {
             <VidScroller vids={video} />
           ))}
 
-          <div style = {evan}>
+          {/* <div style = {evan}>
             <div className="service">
               <FontAwesomeIcon icon={faFilm} style={icon} />
               <h4>Short Films</h4>
@@ -85,88 +85,70 @@ export default class Home extends React.Component {
               <h4>Commericals</h4>
               <p>Get your name out</p>
             </div>  
-            <br/>
+            <br/> */}
 
             <Link href="/work"><button style={button}>View All Work --> </button></Link>
           </div>
-        </div>
         <Footer />
 
         <style jsx>{`
-        /* desktop */
-        @media (min-device-width:1224px) {
-          .service {
-            width: 33%;
-            height: auto;
-            text-align: center;
-            float: left;
+          /* desktop */
+          @media (min-device-width:1224px) {
+            .reel {
+              height: 85vh;
+            }
           }
-        }
-        
-        /* large desktop screen */
-        @media (min-device-width:1824px) {
-          .service {
-            width: 33%;
-            height: auto;
-            text-align: center;
-            float: left;
-          }
-        
-        }
-        
-        /* Small desktop screen */
-        @media (max-width:1224px) {
-          .service {
-            width: 30%;
-            height: auto;
-            text-align: center;
-            float: left;          }
-        
-        }
-        
-        /* Tablet Device */
-        @media (max-device-width:1224px) {
-        @media (orientation: portrait) {
-          .service {
-            width: 30%;
-            height: auto;
-            text-align: center;
-            float: left;          }
-        
-        }
-        @media (orientation: landscape) {
-          .service {
-            width: 30%;
-            height: auto;
-            text-align: center;
-            float: left;          }
-        
-        }
-        }
-        
-        /* Mobile */
-        @media (max-device-width:850px) {
-        @media (orientation: portrait) {
-          .service {
-            width: 100%;
-            height: auto;
-            text-align: center;
-            float: left;
-          }
-        
-        }
-        @media (orientation: landscape) {
-          .service {
-            width: 100%;
-            height: auto;
-            text-align: center;
-            float: left;
-          }
-        
-        }
-        }
 
+          /* large desktop screen */
+          @media (min-device-width:1824px) {
+            .reel {
+              height: 75vh;
+            }
+
+          }
+
+          /* Small desktop screen */
+          @media (max-width:1224px) {
+            .reel {
+              height: 90vh;
+            }
+
+          }
+
+          /* Tablet Device */
+          @media (max-device-width:1224px) {
+          @media (orientation: portrait) {
+            .reel {
+              height: 45vh;
+            }
+
+          }
+          @media (orientation: landscape) {
+            .reel {
+              height: 85vh;
+            }
+
+          }
+          }
+
+          /* Mobile */
+          @media (max-device-width:850px) {
+          @media (orientation: portrait) {
+            .reel {
+              height: 35vh;
+            }
+
+          }
+          @media (orientation: landscape) {
+            .reel {
+              height: 80vh;
+            }
+
+          }
+          }
+        
       `}</style>
+
 
       </>
     )
