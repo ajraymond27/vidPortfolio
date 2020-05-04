4e3660cf87f09668a6dd0865aac2af53ab9b2e14
AJ Raymond <antraymo@iu.edu>
2020-05-04 14:44:16 -0400

    contact info

--- evanoneal/pages/contact.js
+++ evanoneal/pages/contact.js
@@ -1,16 +1,34 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTv } from '@fortawesome/free-solid-svg-icons'
+
 
 const Contact = () => (
   <>
+
   <Header />
   <div className="page"> 
-    <h1>Email</h1>
-    <p>evanjoneal@gmail.com</p>
+    <h1>Contact</h1>
+    <FontAwesomeIcon icon={faTv}/>
+    <span>evanjoneal@gmail.com</span>
     
+    <FontAwesomeIcon icon={faTv}/>
+    <span>555-555-5555</span>
+ 
+    <h1>Connect</h1>
+    <FontAwesomeIcon icon={faTv}/>
+    <span>LinkedIn Link</span>
+    
+    <FontAwesomeIcon icon={faTv}/>
+    <span>Instagram Link</span>
+
+    <FontAwesomeIcon icon={faTv}/>
+    <span>Facebook Link</span>
   </div>
   <Footer />
+
   </>
 )
 
