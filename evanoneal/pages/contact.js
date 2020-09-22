875fda02f1c7d00308b0f719d88aa126607c204e
AJ Raymond <antraymo@iu.edu>
2020-09-22 10:22:43 -0600

    test2

--- evanoneal/pages/contact.js
+++ evanoneal/pages/contact.js
@@ -1,62 +1,62 @@
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
-// import Card from 'react-bootstrap/Card';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-// import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
 
 
-// const icon={
-//   margin: '50px',
-// }
+const icon={
+  margin: '50px',
+}
 
-// const Contact = () => (
-//   <>
+const Contact = () => (
+  <>
 
-//   <div className="page"> 
-//   <Header />
+  <div className="page"> 
+  <Header />
 
-//   <h1>Contact</h1>
-//     <Card >
-//       <Card.Body>
-//       <Card.Subtitle className="mb-2 text-muted">
-//         <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
-//         <span>         evanjoneal@gmail.com</span>
-//       </Card.Subtitle>
-//       <Card.Subtitle className="mb-2 text-muted">
-//         <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
-//         <span>      (317) 556-5848</span>
-//       </Card.Subtitle>
-//       </Card.Body>
-//     </Card>
+  <h1>Contact</h1>
+    <Card >
+      <Card.Body>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
+        <span>         evanjoneal@gmail.com</span>
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
+        <span>      (317) 556-5848</span>
+      </Card.Subtitle>
+      </Card.Body>
+    </Card>
 
-//     <h1>Connect</h1>
-//     <Card >
-//       <Card.Body>
-//       <Card.Subtitle className="mb-2 text-muted">
-//         <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
-//         <span>         evanjoneal@gmail.com</span>
-//       </Card.Subtitle>
-//       <Card.Subtitle className="mb-2 text-muted">
-//         <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
-//         <span>      (317) 556-5848</span>
-//       </Card.Subtitle>
-//       </Card.Body>
-//     </Card>
+    <h1>Connect</h1>
+    <Card >
+      <Card.Body>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faEnvelope} stye={icon}/>
+        <span>         evanjoneal@gmail.com</span>
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        <FontAwesomeIcon icon={faMobileAlt} stye={icon}/>
+        <span>      (317) 556-5848</span>
+      </Card.Subtitle>
+      </Card.Body>
+    </Card>
  
-//     {/* <h1>Connect</h1>
-//     <FontAwesomeIcon icon={faTv}/>
-//     <span>LinkedIn Link</span>
+    {/* <h1>Connect</h1>
+    <FontAwesomeIcon icon={faTv}/>
+    <span>LinkedIn Link</span>
     
-//     <FontAwesomeIcon icon={faTv}/>
-//     <span>Instagram Link</span>
+    <FontAwesomeIcon icon={faTv}/>
+    <span>Instagram Link</span>
 
-//     <FontAwesomeIcon icon={faTv}/>
-//     <span>Facebook Link</span> */}
-//   </div>
-//   <Footer />
+    <FontAwesomeIcon icon={faTv}/>
+    <span>Facebook Link</span> */}
+  </div>
+  <Footer />
 
-//   </>
-// )
+  </>
+)
 
-// export default Contact
+export default Contact
